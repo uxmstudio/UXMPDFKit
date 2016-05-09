253bf25546f8a50857754904e209e09eb7fd8841
Chris Anderson <hi@chrisanderson.io>
2016-05-08 19:18:51 -0500

    Added annotation store

---
+++ Pod/Classes/PDFAnnotationStore.swift
@@ -0,0 +1,48 @@
+//
+//  PDFAnnotationStore.swift
+//  Pods
+//
+//  Created by Chris Anderson on 5/8/16.
+//
+//
+
+import UIKit
+
+public class PDFAnnotationStore {
+    
+    var pages:[Int:PDFAnnotationPage] = [:]
+    
+    func addAnnotation(annotation: PDFAnnotation, page: Int) {
+        
+        if let storePage:PDFAnnotationPage = pages[page] {
+            
+            storePage.addAnnotation(annotation)
+        }
+        else {
+            
+            var storePage = PDFAnnotationPage()
+            storePage.addAnnotation(annotation)
+            storePage.page = page
+            pages[page] = storePage
+        }
+    }
+    
+    func drawAnnotations(page: Int, context:CGContextRef) {
+        
+        if let storePage = pages[page] {
+            for annotation in storePage.annotations {
+                annotation.drawInContext(context)
+            }
+        }
+    }
+}
+
+public class PDFAnnotationPage {
+    
+    var annotations:[PDFAnnotation] = []
+    var page:Int = 0
+    
+    func addAnnotation(annotation: PDFAnnotation) {
+        annotations.append(annotation)
+    }
+}
