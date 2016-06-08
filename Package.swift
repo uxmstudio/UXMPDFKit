cf4474abde20bc176d6b12805d414d8573b7ecb6
Chris Anderson <hi@chrisanderson.io>
2016-06-08 15:31:07 -0500

    Added Swift Package Manager file

---
+++ Package.swift
@@ -0,0 +1,13 @@
+//
+//  Package.swift
+//  UXMPDFKit
+//
+//  Created by Chris Anderson on 6/8/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "UXMPDFKit"
+)
