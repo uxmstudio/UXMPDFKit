5e0e23d4404d9a704edbebb31803e7097c5deb1e
Chris Anderson <hi@chrisanderson.io>
2017-02-14 11:49:18 -0600

    Fixes #38

--- Pod/Classes/Extensions/UIImage.swift
+++ Pod/Classes/Extensions/UIImage.swift
@@ -24,4 +24,19 @@ extension UIImage {
         }
         return image
     }
+    
+    func scaleImage(toSize newSize: CGSize) -> UIImage? {
+        let newRect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height).integral
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 0)
+        if let context = UIGraphicsGetCurrentContext() {
+            context.interpolationQuality = .high
+            let flipVertical = CGAffineTransform(a: 1, b: 0, c: 0, d: -1, tx: 0, ty: newSize.height)
+            context.concatenate(flipVertical)
+            context.draw(self.cgImage!, in: newRect)
+            let newImage = UIImage(cgImage: context.makeImage()!)
+            UIGraphicsEndImageContext()
+            return newImage
+        }
+        return nil
+    }
 }
