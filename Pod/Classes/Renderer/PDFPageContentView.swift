ac571ae7221f6ac28c0f3286be5704e54729654a
Daniel Morandini <danielmorandini@me.com>
2017-05-23 17:08:44 +0200

    Add a getter for pageContentView's contentView

--- Pod/Classes/Renderer/PDFPageContentView.swift
+++ Pod/Classes/Renderer/PDFPageContentView.swift
@@ -17,28 +17,32 @@ public protocol PDFPageContentViewDelegate {
 open class PDFPageContentView: UIScrollView, UIScrollViewDelegate {
     let contentView: PDFPageContent
     let containerView: UIView
-    
+
+    open var pdfContentView: UIView {
+        return contentView as UIView
+    }
+
     open var page: Int
     open var contentDelegate: PDFPageContentViewDelegate?
     open var viewDidZoom: ((CGFloat) -> Void)?
     fileprivate var PDFPageContentViewContext = 0
     fileprivate var previousScale: CGFloat = 1.0
-    
+
     let bottomKeyboardPadding: CGFloat = 20.0
-    
+
     init(frame: CGRect, document: PDFDocument, page: Int) {
         self.page = page
         contentView = PDFPageContent(document: document, page: page)
-        
+
         containerView = UIView(frame: contentView.bounds)
         containerView.isUserInteractionEnabled = true
         containerView.contentMode = .redraw
         containerView.backgroundColor = UIColor.white
         containerView.autoresizesSubviews = true
         containerView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
-        
+
         super.init(frame: frame)
-        
+
         scrollsToTop = false
         delaysContentTouches = false
         showsVerticalScrollIndicator = false
@@ -53,19 +57,19 @@ open class PDFPageContentView: UIScrollView, UIScrollViewDelegate {
         isScrollEnabled = true
         clipsToBounds = true
         autoresizingMask = [.flexibleHeight, .flexibleWidth]
-        
+
         contentView.translatesAutoresizingMaskIntoConstraints = false
         contentSize = contentView.bounds.size
-        
+
         containerView.addSubview(contentView)
         addSubview(containerView)
-        
+
         updateMinimumMaximumZoom()
-        
+
         zoomScale = minimumZoomScale
         tag = page
-        
-        
+
+
         NotificationCenter.default.addObserver(
             self,
             selector: #selector(PDFPageContentView.keyboardWillShowNotification(_:)),
@@ -77,7 +81,7 @@ open class PDFPageContentView: UIScrollView, UIScrollViewDelegate {
             name: .UIKeyboardWillHide,
             object: nil
         )
-        
+
         let singleTapRecognizer = UITapGestureRecognizer(
             target: self,
             action: #selector(PDFPageContentView.processSingleTap(_:))
@@ -91,59 +95,59 @@ open class PDFPageContentView: UIScrollView, UIScrollViewDelegate {
     required public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     deinit {
         NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow, object: nil)
         NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
     }
-    
+
     override open func layoutSubviews() {
         super.layoutSubviews()
-        
+
         let boundsSize = bounds.size
         var viewFrame = containerView.frame
-        
+
         if viewFrame.size.width < boundsSize.width {
             viewFrame.origin.x = (boundsSize.width - viewFrame.size.width) / 2.0 + contentOffset.x
         } else {
             viewFrame.origin.x = 0.0
         }
-        
+
         if viewFrame.size.height < boundsSize.height {
             viewFrame.origin.y = (boundsSize.height - viewFrame.size.height) / 2.0 + contentOffset.y
         } else {
             viewFrame.origin.y = 0.0
         }
-        
+
         containerView.frame = viewFrame
         contentView.frame = containerView.bounds
     }
-    
-    
+
+
     override open func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         guard context == &PDFPageContentViewContext else {
             return
         }
-        
+
         guard let keyPath = keyPath , keyPath == "frame" else {
             return
         }
-        
+
         guard self == (object as? PDFPageContentView) else {
             return
         }
-        
+
         let oldMinimumZoomScale = minimumZoomScale
-        
+
         updateMinimumMaximumZoom()
-        
+
         if zoomScale == oldMinimumZoomScale || zoomScale < minimumZoomScale {
             zoomScale = minimumZoomScale
         } else if (zoomScale > maximumZoomScale) {
             zoomScale = maximumZoomScale
         }
     }
-    
+
     open func processSingleTap(_ recognizer: UITapGestureRecognizer) {
         if let action = contentView.processSingleTap(recognizer) as? PDFAction {
             contentDelegate?.contentView(self, didSelect: action)
@@ -155,72 +159,72 @@ open class PDFPageContentView: UIScrollView, UIScrollViewDelegate {
             contentDelegate?.contentView(self, tapped: recognizer)
         }
     }
-    
+
     open override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
         let result = super.hitTest(point, with: event)
         self.isScrollEnabled = !(result is ResizableBorderView)
         return result
     }
-    
-    
+
+
     //MARK: - Zoom methods
     open func zoomIncrement() {
         var zoomScale = self.zoomScale
-        
+
         if zoomScale < minimumZoomScale {
             zoomScale /= 2.0
-            
+
             if zoomScale > minimumZoomScale {
                 zoomScale = maximumZoomScale
             }
-            
+
             setZoomScale(zoomScale, animated: true)
         }
     }
-    
+
     open func zoomDecrement() {
         var zoomScale = self.zoomScale
-        
+
         if zoomScale < minimumZoomScale {
             zoomScale *= 2.0
-            
+
             if zoomScale > minimumZoomScale {
                 zoomScale = maximumZoomScale
             }
-            
+
             setZoomScale(zoomScale, animated: true)
         }
     }
-    
+
     open func zoomReset() {
         if zoomScale > minimumZoomScale {
             zoomScale = minimumZoomScale
         }
     }
-    
+
     //MARK: - UIScrollViewDelegate methods
     open func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         return containerView
     }
-    
+
     open func scrollViewDidZoom(_ scrollView: UIScrollView) {
         viewDidZoom?(scrollView.zoomScale)
     }
-    
+
     func keyboardWillShowNotification(_ notification: Notification) {
         updateBottomLayoutConstraintWithNotification(notification, show: true)
     }
-    
+
     func keyboardWillHideNotification(_ notification: Notification) {
         updateBottomLayoutConstraintWithNotification(notification, show: false)
     }
-    
+
     func updateBottomLayoutConstraintWithNotification(_ notification: Notification, show:Bool) {
         let userInfo = (notification as NSNotification).userInfo!
-        
+
         let keyboardEndFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
         let convertedKeyboardEndFrame = self.convert(keyboardEndFrame, from: self.window)
-        
+
         let height: CGFloat
         if convertedKeyboardEndFrame.height > 0 && show {
             height = convertedKeyboardEndFrame.height + bottomKeyboardPadding
@@ -230,20 +234,20 @@ open class PDFPageContentView: UIScrollView, UIScrollViewDelegate {
 
         contentInset = UIEdgeInsetsMake(0, 0, height, 0)
     }
-    
-    
+
+
     //MARK: - Helper methods
     static func zoomScaleThatFits(_ target: CGSize, source: CGSize) -> CGFloat {
         let widthScale = target.width / source.width
         let heightScale = target.height / source.height
         return (widthScale < heightScale) ? widthScale : heightScale
     }
-    
+
     func updateMinimumMaximumZoom() {
         previousScale = self.zoomScale
         let targetRect = bounds.insetBy(dx: 0, dy: 0)
         let zoomScale = PDFPageContentView.zoomScaleThatFits(targetRect.size, source: contentView.bounds.size)
-        
+
         minimumZoomScale = zoomScale
         maximumZoomScale = zoomScale * 16.0
     }
