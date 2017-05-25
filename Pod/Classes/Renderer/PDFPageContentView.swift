uxmstudio/UXMPDFKit

commit 508d426aef28c98f135c466f27565804311b8e2b

Introduces fixes to aid in display size changes Fixes #4

Pod/Classes/Renderer/PDFPageContentView.swift
@@ -52,6 +52,7 @@ open class PDFPageContentView: UIScrollView, UIScrollViewDelegate {
         delegate = self
         isScrollEnabled = true
         clipsToBounds = true
+        
         autoresizingMask = [.flexibleHeight, .flexibleWidth]
         
         contentView.translatesAutoresizingMaskIntoConstraints = false
@@ -111,12 +112,17 @@ open class PDFPageContentView: UIScrollView, UIScrollViewDelegate {
         
         if viewFrame.size.height < boundsSize.height {
             viewFrame.origin.y = (boundsSize.height - viewFrame.size.height) / 2.0 + contentOffset.y
-        } else {
+        }
+        else {
             viewFrame.origin.y = 0.0
         }
-        
+
         containerView.frame = viewFrame
         contentView.frame = containerView.bounds
+        
+        updateMinimumMaximumZoom()
+        
+        self.zoomReset()
     }
     
     
@@ -193,9 +199,13 @@ open class PDFPageContentView: UIScrollView, UIScrollViewDelegate {
     }
     
     open func zoomReset() {
-        if zoomScale > minimumZoomScale {
-            zoomScale = minimumZoomScale
-        }
+        zoomScale = minimumZoomScale
+        
+        let offsetX = max((self.bounds.size.width - self.contentSize.width) * 0.5, 0.0)
+        let offsetY = max((self.bounds.size.height - self.contentSize.height) * 0.5, 0.0)
+        
+        containerView.center = CGPoint(x: self.contentSize.width * 0.5 + offsetX,
+                                       y: self.contentSize.height * 0.5 + offsetY)
     }
     
     //MARK: - UIScrollViewDelegate methods

Pod/Classes/Renderer/PDFSinglePageViewer.swift
@@ -130,8 +130,7 @@ extension PDFSinglePageViewer: UICollectionViewDataSource {
         let contentFrame = CGRect(origin: CGPoint.zero, size: contentSize)
         
         let page = indexPath.row + 1
-        
-        cell.contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+
         cell.pageContentView = PDFPageContentView(frame: contentFrame, document: document!, page: page)
         cell.pageContentView?.contentDelegate = self
         
@@ -211,9 +210,6 @@ extension PDFSinglePageViewer: UIScrollViewDelegate {
             let currentlyShownIndexPath = indexPathsForVisibleItems.first ?? IndexPath(item: 0, section: 0)
             page = currentlyShownIndexPath.row + 1
         }
-        
-        print(page)
-        print(internalPage)
         
         /// If nothing has changed, dont reload
         if page == internalPage {

Pod/Classes/Renderer/PDFViewController.swift
@@ -131,17 +131,20 @@ open class PDFViewController: UIViewController {
         pageScrubber.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         pageScrubber.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
         
-        automaticallyAdjustsScrollViewInsets = false
-        
         pageScrubber.sizeToFit()
         
         reloadBarButtons()
     }
     
+    open override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        collectionView.collectionViewLayout.invalidateLayout()
+    }
+    
     open override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
-        collectionView.contentInset = UIEdgeInsetsMake(topLayoutGuide.length, 0, bottomLayoutGuide.length, 0)
         collectionView.collectionViewLayout.invalidateLayout()
         
         view.layoutSubviews()
@@ -281,8 +284,8 @@ open class PDFViewController: UIViewController {
     
     /// Toggles the display of the navigation bar and scrubber bar
     func toggleBars() {
-        hideBars(state: !(navigationController?.isNavigationBarHidden ?? false))
         collectionView.collectionViewLayout.invalidateLayout()
+        hideBars(state: !(navigationController?.isNavigationBarHidden ?? false))
     }
     
     //MARK: - IBActions

commit 3be79949321d9e3f837476bfe6b9170949c1495f

Fixes broken zooming caused by 508d426

Pod/Classes/Renderer/PDFPageContentView.swift
@@ -66,6 +66,7 @@ open class PDFPageContentView: UIScrollView, UIScrollViewDelegate {
         zoomScale = minimumZoomScale
         tag = page
         
+        self.addObserver(self, forKeyPath: "frame", options: [.new, .old], context: &PDFPageContentViewContext)
         
         NotificationCenter.default.addObserver(
             self,
@@ -96,6 +97,8 @@ open class PDFPageContentView: UIScrollView, UIScrollViewDelegate {
     deinit {
         NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow, object: nil)
         NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
+        
+        self.removeObserver(self, forKeyPath: "frame")
     }
     
     override open func layoutSubviews() {
@@ -119,35 +122,18 @@ open class PDFPageContentView: UIScrollView, UIScrollViewDelegate {
 
         containerView.frame = viewFrame
         contentView.frame = containerView.bounds
-        
-        updateMinimumMaximumZoom()
-        
-        self.zoomReset()
     }
     
-    
     override open func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-        guard context == &PDFPageContentViewContext else {
+        
+        guard context == &PDFPageContentViewContext,
+            let keyPath = keyPath, keyPath == "frame",
+            self == (object as? PDFPageContentView) else {
             return
         }
-        
-        guard let keyPath = keyPath , keyPath == "frame" else {
-            return
-        }
-        
-        guard self == (object as? PDFPageContentView) else {
-            return
-        }
-        
-        let oldMinimumZoomScale = minimumZoomScale
         
         updateMinimumMaximumZoom()
-        
-        if zoomScale == oldMinimumZoomScale || zoomScale < minimumZoomScale {
-            zoomScale = minimumZoomScale
-        } else if (zoomScale > maximumZoomScale) {
-            zoomScale = maximumZoomScale
-        }
+        self.zoomReset()
     }
     
     open func processSingleTap(_ recognizer: UITapGestureRecognizer) {